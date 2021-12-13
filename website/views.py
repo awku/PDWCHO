e5bb03dacdd72f8f9a9013a20d7556a6ceb064ad
Aleksandra <aleksandra0kus@gmail.com>
2021-12-13 23:28:27 +0100

    followed/rating fixes

--- website/views.py
+++ website/views.py
@@ -227,7 +227,7 @@ def user_view(request):
         user_data = user_data[0]
         if request.method == "POST":
             app.create_following(user1_id, user2_id)
-        is_followed = 'followed' if app.is_user_followed(user1_id, user2_id) else 'not followed'
+        is_followed = 'followed' if app.is_user_followed(user1_id, user2_id)[0] else 'not followed'
         return render(request, 'user.html', {'user': user_data, 'can_follow': is_followed})
     else:
         return render(request, 'error.html')
